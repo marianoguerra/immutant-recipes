0dae444f82d8570341ca774ca207cfc641ca2de2
Mariano Guerra <luismarianoguerra@gmail.com>
2012-12-21 15:25:44 +0100

    add a sync version of the handler to compare

--- async-handler/src/immutant/init.clj
+++ async-handler/src/immutant/init.clj
@@ -3,8 +3,13 @@
     [async.core :as async]
     [immutant.web :as web])) 
 
+(defn sync-handler [request]
+  (Thread/sleep 5000)
+  {:status 200 :body "finished"})
+
 (defn async-handler [request on-complete]
   (Thread/sleep 5000)
   (on-complete {:status 200 :body "finished"}))
 
-(web/start "/" (async/handle-async async-handler))
+(web/start "/async" (async/handle-async async-handler))
+(web/start "/sync" sync-handler)
